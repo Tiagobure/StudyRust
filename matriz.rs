4cfd797a6e304db89bd2446017125c87a27a5dc4
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 13:35:29 -0300

    Rust array

---
+++ matriz.rs
@@ -0,0 +1,13 @@
+fn main(){
+
+    let matriz_1 :[u32; 5] = [10,11,12,13,14];
+
+    let matriz_2 = ['a','b','c','d','e'];
+
+    println!("o valor selecionado foi : {}", matriz_1[4]);
+    println!("o valor selecionado foi : {}", matriz_2[4]);
+
+
+
+
+}
