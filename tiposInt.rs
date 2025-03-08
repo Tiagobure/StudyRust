1ec265911dde11701135bb0beedf4199a3877eb1
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 12:30:44 -0300

    Rust TiposINT

---
+++ tiposInt.rs
@@ -0,0 +1,9 @@
+fn main(){
+    
+    //let salario :i32 = 4294967295;
+
+    //let signed_64 :i64 =9211654684648648;
+    let signed_64 :usize =18446744073709551615;
+
+    println!(" {} ", signed_64);
+}
