19fc4ede52bb3f990751f400b81bc735242ed831
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 14:17:17 -0300

    for, loop, while

---
+++ fwl.rs
@@ -0,0 +1,29 @@
+
+//for: é quantidade.
+    //while: é condição.
+    //loop: repetição infinita.
+fn main(){
+
+     
+    
+    let mut x = 0;
+    loop{
+        println!("Esse é o numero {}", x);
+        if x == 29{
+            println!("Esse é o fim");
+            break;
+            //continue;
+        }
+        x = x + 1;
+    }
+    
+    //let mut x = 0;
+       // while x < 1000{
+       //     println!("Esse é o numero:{} ", x);
+        //    x= x + 1;
+      //  }
+
+  //  for x in 0..5{
+      //println!("Esse é o caminho {} ", x);
+    //}
+}
