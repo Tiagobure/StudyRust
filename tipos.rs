9b63021343ff9426f324a2203f4d4cbcd6e30d67
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 12:11:06 -0300

    Rust Tipos

---
+++ tipos.rs
@@ -0,0 +1,16 @@
+fn main(){
+   // let tipo1= 'c';
+    //let tipo2 = "Linguagem rust";
+    //let tipo3 = 1234;
+    //let tipo4 = true;
+     let tipo5 = 22_534.32;
+
+
+//println!("Dados do tipo char é: {}", tipo1);
+//println!("Dados do tipo String é: {}", tipo2);
+//println!("Dados do tipo int é: {}", tipo3);
+//println!("Dados do tipo boolean é: {}", tipo4);
+println!("Dados do tipo float é: {}", tipo5);
+
+
+}
