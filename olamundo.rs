522ade6def02a3df0b0d68bdd080c21488d04817
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 12:00:04 -0300

    Rust name vars

--- olamundo.rs
+++ olamundo.rs
@@ -1,4 +1,14 @@
 fn main(){
-    print!("Olá mundo");
-   // println!("Olá mundo ")
+    //Regra 1 começar com letras minusculas
+    //regra 2 terminar com letras minusculas ou numeros
+    //obs underscore_ pode ser usado entre as letras e numero
+
+    // let msg =  "olá mundo";
+    // let msg1 = "olá mundo";
+
+   //let m_1_s_2_g = "olá mundo";
+   //let _msg = "Olá mundo";
+   let _1msg = "olá mundo";
+   println!("Sua mensagem é: {} ", _1msg);
+
 }
