e7f89b160e08b38cccb9d5425760024d9eb077ce
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 13:39:40 -0300

    Rust condicional if/else

---
+++ condicional.rs
@@ -0,0 +1,11 @@
+fn main(){
+
+    let salario :u32 = 1200;
+
+    if salario == 1200{
+        println!("seu salario é o minimo");
+    }else{
+        println!("seu salario é desconhecido");
+
+    }
+}
