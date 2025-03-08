d483f4041f34c8964fb5a410968a6c1266f3a88c
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 13:59:24 -0300

    Rust relacionais

---
+++ relacionais.rs
@@ -0,0 +1,14 @@
+fn main(){
+        //operadores relacionais
+
+        let a :u32 = 15;
+        let b :u32  = 15;
+
+        //< > <= >= != ==
+        if a == b{
+            println!("Condição aceita!");
+        }else{
+            println!("Condição negada");
+        }
+
+}
