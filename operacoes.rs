499d2d9f19d9c16c6b5240a3fc060417a8a1ad28
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 13:09:08 -0300

    Rust operacoes

---
+++ operacoes.rs
@@ -0,0 +1,26 @@
+fn main(){
+       
+    let a = 20;
+    let b = 4;
+    let c = 6;
+
+    let soma= a+b+c;
+    let subtracao = a-b-c;
+    let divisao = a/b;
+    let multiplicacao = a*b*c;
+    let resto = a % c;
+
+    println!("Resultado para soma é: {}", a+b+c);
+    println!("Resultado para subtração é: {}", a-b-c);
+    println!("Resultado para divisão é: {}", a/b);
+    println!("Resultado para multiplicação é: {}", a*b*c);
+    println!("Resultado para resto é: {}", a % c);
+
+    println!("Soma: {}", soma);
+    println!("Subtração: {}", subtracao);
+    println!("divisão: {}", divisao);
+    println!("Multiplicacão: {}", multiplicacao);
+    println!("Resto: {}", resto);
+
+
+}
