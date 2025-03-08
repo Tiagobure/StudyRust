c5bf5606ad7f2bfcdae60a0928922a2f901f701c
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 12:42:09 -0300

    Rust constantes

---
+++ constantes.rs
@@ -0,0 +1,14 @@
+//constante Global
+const CONST_1 :char = 'c';
+
+fn main(){
+
+    //const CONST_1 :char = 'c';
+ const CONST_2 :&str ="Palio";
+    println!(" {} ", CONST_1);
+    println!("{}", CONST_2);
+
+
+
+
+}
--- tiposInt.rs
+++ tiposInt.rs
@@ -4,6 +4,6 @@ fn main(){
 
     //let signed_64 :i64 =9211654684648648;
     let signed_64 :usize =18446744073709551615;
-
+//limite baseado no sistema operacional
     println!(" {} ", signed_64);
 }
