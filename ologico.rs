9a882638a9022034b6de398f26a6c8de7f531f56
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 13:54:12 -0300

    Rust &&, ||, !

---
+++ ologico.rs
@@ -0,0 +1,21 @@
+fn main(){
+//e &&, ou ||, nao !
+
+//let fruta :&str = "banana";
+//let estado_fruta :&str = "Madura";
+
+//if fruta == "abacate" || {fruta == "banana"} && 
+//{estado_fruta == "Madura"}{
+   // println!("vamos fazer uma vitamina");
+//} else{
+   // println!("não vou fazer a vitamina");
+//}
+
+let salario :u32 = 3000;
+if !{salario == 12000}{
+    println!("Você é um mentiroso");
+} else{
+    println!("você falou a verdade");
+}
+
+}
