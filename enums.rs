8ac83d0a8e754535b0612b1045620578e3c05d31
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-03-08 14:57:15 -0300

    Rust enum

---
+++ enums.rs
@@ -0,0 +1,15 @@
+fn main(){
+        let meu_pet: Animal= Animal::Gato;
+        match meu_pet{
+            Animal::Cachorro => println!("O seu animal é um cachorro"),
+            Animal::Gato => println!("O seu animal é um gato"),
+            Animal::Calopsita => println!("O seu animal é um passaro"),
+            _=> println!("O seu animal é desconhecido"),
+        }
+}
+
+enum Animal{
+    Cachorro,
+    Gato,
+    Calopsita,
+}
